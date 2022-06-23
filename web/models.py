38079834e641903dd835c93c52c8ae4d2762c165
RojaVictoria <82482780+RojaVictoria@users.noreply.github.com>
2022-06-23 02:25:30 -0400

    Hito 3 hasta requerimiento 2

--- web/models.py
+++ web/models.py
@@ -1,3 +1,11 @@
+import uuid
 from django.db import models
 
-# Create your models here.
+
+class Flan(models.Model):
+    flan_uuid = models.UUIDField(default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=64)
+    description = models.TextField()
+    image_url = models.URLField()
+    slug = models.SlugField(max_length=50, allow_unicode=True)
+    is_private = models.BooleanField()
--- web/views.py
+++ web/views.py
@@ -1,10 +1,20 @@
 from django.shortcuts import render
+from .models import Flan
+
 
 def index(request):
-    return render(request, 'index.html')
+    flanes_publicos = Flan.objects.filter(is_private=False)
+    diccionario = {
+        'flanes_publicos': flanes_publicos,
+    }
+    return render(request, 'index.html', diccionario)
 
 def about(request):
     return render(request, 'about.html')
 
 def welcome(request):
-    return render(request, 'welcome.html')
+    flanes_privados = Flan.objects.filter(is_private=True)
+    diccionario = {
+        'flanes_privados': flanes_privados,
+    }
+    return render(request, 'welcome.html', diccionario)
